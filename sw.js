0737d1bab3847d52f2811dda1e15a6feb2674c16
vic lotana <viclotana@gmail.com>
2018-07-19 15:37:17 +0100

    done2

--- sw.js
+++ sw.js
@@ -1,5 +1,14 @@
-self.addEventListener('install', e => {
-    console.log(`install`);
+//cache all ststic assets
+const staticAssets = [
+  '/',
+  './main.css',
+  './app.js'
+];
+
+self.addEventListener('install', async e => {
+    //console.log(`install`);
+    const cache = await caches.open('news-static');
+    cache.addAll(staticAssets);
 });
 
 self.addEventListener('fetch', e => {
