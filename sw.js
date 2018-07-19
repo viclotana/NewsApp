7436d5e420dfd3a58b97858944c6e2f09d4c1324
vic lotana <viclotana@gmail.com>
2018-07-19 16:14:54 +0100

    offline content

--- sw.js
+++ sw.js
@@ -14,10 +14,27 @@ self.addEventListener('install', async e => {
 self.addEventListener('fetch', e => {
     //console.log(`fetch`);
     const req = e.request;
+    const url = new URL(req.url);
+
+    if(url.origin === location.origin){
     e.respondWith(cacheFirst(req));
+    } else {
+      e.respondWith(networkFirst(req)); 
+    }
 });
 
 async function cacheFirst(req) {
     const cachedResponse = await caches.match(req);
     return cachedResponse || fetch(req);
+}
+
+async function networkFirst(req){
+    const cache = await caches.open('news-dynamic');
+    try {
+        const res = await fetch(req);
+        cache.put(req, res.clone());
+        return res;
+    } catch (error) {
+        return await cache.match(req);
+    }
 }
