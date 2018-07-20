f015d4a6c23faf108f9956cc92b237b20df99541
vic lotana <viclotana@gmail.com>
2018-07-20 15:13:53 +0100

    calling workbox from cdn

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ window.addEventListener('load', async e => {
 
     if('serviceWorker' in navigator){
       try {
-        navigator.serviceWorker.register('sw.js');
+        navigator.serviceWorker.register('./sw.js');
         console.log(`SW registered`);
       } catch (error) {
         console.log(`SW reg failed`);
--- sw.js
+++ sw.js
@@ -1,9 +1,34 @@
-importScripts('./node_modules/workbox-sw/build/workbox-sw.js')
-//cache all ststic assets
-const staticAssets = [
-  '/',
+//const staticAssets = [
+ // '/',
+ // './main.css',
+ // './app.js',
+ // './fallback.json',
+ // './Buhari1.jpg'
+//];
+
+//const wb = new WorkboxSW();
+
+//workbox.precaching(staticAssets);
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
+
+workbox.routing.registerRoute(
+  new RegExp('https://newsapi.org/(.*)'),
+  workbox.strategies.networkFirst({
+    // Use a custom cache for this route
+    cacheName: 'dynamic-cache',
+    plugins: [
+     
+    ]
+}));
+
+
+
+workbox.precaching.precacheAndRoute([
   './main.css',
   './app.js',
-  './fallback.json',
-  './Buhari1.jpg'
-];
+  { url: '/index.html',
+    revision: '001' },
+]);
+
+workbox.skipWaiting();
+workbox.clientsClaim();
